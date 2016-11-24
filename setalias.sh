e7ace0cc0b56652b9c514ceeff9fdf901e6881f2
Pratulya Bubna <pratulyabubna@outlook.com>
2016-11-24 12:01:13 +0530

    Add path instructions

--- setalias.sh
+++ setalias.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Shell script to set alias 'song' for running the script
+# It is important to execute this file with 'source'
+
 echo -e "Note:\tTo permanently set the alias, pass in the '-p' option\t\tsource setalias.sh -p"
 
 ALIAS="song" # Set the alias to this
 UNALIAS=("alias1" "alias2")
-PY_PATH="/Users/Pratulya/Desktop/Later/PythonScripts/youtube-mp3/download.py" # Path to the script
-### DON'T FORGET TO BACKSLASH THE METACHARS USED IN PATHS ###
+PY_PATH="/path/to/python/script/download.py" # Path to the script
+### DON'T FORGET TO BACKSLASH THE METACHARS USED IN PATHS, IF ANY ###
 ### IMPORANT TO PROVIDE ABSOLUTE PATH ###
-DESTINATION="/Users/Pratulya/Downloads" # Save the downloaded mp3 file to this directory
+DESTINATION="/absolute/path/to/destination/folder" # Save the downloaded mp3 file to this directory
 
 PY_CMD="python3"
 
@@ -27,7 +29,7 @@ if [ ! -d "$DESTINATION" ]; then
 	echo "Destination directory does not exist"
 	read -ep "Would like to create the directory? (y/n)\t" verdict
 	CHOICES=( "y", "Y", "Yes", "yes", "YES" )
-	if [ verdict in $CHOICES ]; then
+	if [ verdict in ${CHOICES[@]} ]; then
 		mkdir $DESTINATION
 		if [ $? ]; then
 			echo "Directory "$DESTINATION" has been successfully created!"
@@ -48,6 +50,7 @@ FINAL="$PY_CMD $PY_PATH --p=\\\"$DESTINATION\\\""
 alias $ALIAS="$FINAL"
 echo "Alias $ALIAS has been set"
 
+# Appending the alias command to bash config file, if "-p" is passed in
 if [[ $# != 0 ]] && [[ "$1" == "-p" ]]; then
 	FINAL="alias $ALIAS=\"$FINAL\""
 	if [ -f ~/.bash_profile ]; then
